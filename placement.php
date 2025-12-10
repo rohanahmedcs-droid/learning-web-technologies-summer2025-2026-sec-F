08ecb2533ccdc2e956452fae95910f9c8aab03fd
Ahmed Rohan <rohanahmed.cs@gmail.com>
2025-12-10 14:31:03 +0600

    Add files via upload

---
+++ placement.php
@@ -0,0 +1,17 @@
+<?php
+$length = 10;
+$width = 5;
+$area = $length * $width;
+$perimeter = 2 * ($length * $width);
+
+echo "area of rectangle = $area <br>";
+echo "perimeter of rectangle =$perimeter <br><br>";
+?>
+
+<?php
+$amount = 1000;
+$vat = $amount * 0.15;
+
+echo "Amount =$amount<br>";
+echo "VAT(15%) =$vat<br><br>";
+?>
